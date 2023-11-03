c57a5f16d1a79f9730d0d6e906d99fe9c1ae1fd7
LouiseUniverse <universe9462@gmail.com>
2023-11-03 11:09:03 +0800

    week08

--- week08/week08-2.py
+++ week08/week08-2.py
@@ -1,7 +1,7 @@
 a=[1,3,5,7,9,11,13]  #數字範圍是死的
-for i in range(7):
+for i in range(4):
   print('第一個數字是', a[i])
-
+print('上面數字範圍是死的 range(4)就只到第四個')
 N = len(a)     #數字範圍是活的
 for i in range(N):
   print('第一個數字是',a[i])
