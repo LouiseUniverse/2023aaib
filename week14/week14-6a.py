b3828a6b70664e142918a17301a82ff820681dc8
LouiseUniverse <universe9462@gmail.com>
2023-12-15 11:52:40 +0800

    week14

---
+++ week14/week14-6a.py
@@ -0,0 +1,12 @@
+#1
+#a= int(input())
+#print(abs(a-2),end='')
+
+
+
+#2
+a = int(input())
+if a-2 > 0:
+	print( a - 2,end='' )
+else:
+	print( 2 - a,end='' )
---
+++ week14/week14-6b.py
@@ -0,0 +1,14 @@
+#1
+#a= int(input())
+#if a==2: print(28,end='')
+#elif a==4 or a==6 or a==9 or a==11: print(30, end='')
+#else: print(31,end='')
+
+
+
+
+
+#2
+a=int(input())
+ans = (0, 31,28,31,30,31,30,31,31,30,31,30,31)
+print(ans[a],end='')
